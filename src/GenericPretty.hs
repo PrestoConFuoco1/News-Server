ec14ea4377181f1cde4d4da5492d827c2e2b2292
jojo bean <keepersofsecrets1@gmail.com>
2021-09-17 23:33:28 +0300

    Added generic functions to ensure that editing types are correct; it means that there can't be all optional fields nothing

--- src/GenericPretty.hs
+++ src/GenericPretty.hs
@@ -217,3 +217,81 @@ instance (Selector s, GPrettyShow f) => GPrettyShowAux (S1 s f) where
 
 --------------------------------------------------------------------
 
+
+correctMaybes :: (Generic a, CorrectMaybes (Rep a)) => a -> Bool
+correctMaybes x = case gCorrectMaybes . from $ x
+    of Nothing -> True
+       Just x  -> x
+
+class CorrectMaybes f where
+    gCorrectMaybes :: f a -> Maybe Bool
+
+instance (CorrectMaybes f) => CorrectMaybes (D1 d f) where
+    gCorrectMaybes (M1 x) = gCorrectMaybes x
+
+instance (CorrectMaybes f) => CorrectMaybes (C1 d f) where
+    gCorrectMaybes (M1 x) = gCorrectMaybes x
+
+instance (CorrectMaybes f, CorrectMaybes g) => CorrectMaybes ((:*:) f g) where
+    gCorrectMaybes (x :*: y) = gCorrectMaybes x `q` gCorrectMaybes y
+      where q Nothing x = x
+            q x Nothing = x
+            q (Just x) (Just y) = Just $ x || y
+
+instance (CorrectMaybes f) => CorrectMaybes (S1 d f) where
+    gCorrectMaybes (M1 x) = gCorrectMaybes x
+
+instance (CorrectMaybes' c) => CorrectMaybes (Rec0 c) where
+    gCorrectMaybes (K1 x) = correctMaybes' x
+
+
+--instance CorrectMaybes1 
+class CorrectMaybes' a where
+    correctMaybes' :: a -> Maybe Bool
+
+   -- default correctMaybes' :: (Generic a, CorrectMaybes1 (Rep a)) => a -> Bool
+    default correctMaybes' :: a -> Maybe Bool
+    --correctMaybes' = gCorrectMaybes1 . from
+    correctMaybes' = const Nothing
+
+instance CorrectMaybes' Int
+instance CorrectMaybes' T.Text
+instance CorrectMaybes' TL.Text
+
+instance CorrectMaybes' (Maybe a) where
+    correctMaybes' = gCorrectMaybes1 . from
+
+
+class CorrectMaybes1 f where
+    gCorrectMaybes1 :: f a -> Maybe Bool
+-- {-
+
+instance (CorrectMaybes1 f, Datatype d) => CorrectMaybes1 (D1 d f) where
+    gCorrectMaybes1 z@(M1 x) =
+        if datatypeName z == "Maybe"
+        then gCorrectMaybes1 x
+        else Nothing
+
+instance (Constructor d) => CorrectMaybes1 (C1 d f) where
+    gCorrectMaybes1 z@(M1 x) = Just $ conName z /= "Nothing"
+
+instance (CorrectMaybes1 f, CorrectMaybes1 g) => CorrectMaybes1 ((:+:) f g) where
+    gCorrectMaybes1 (L1 x) = gCorrectMaybes1 x
+    gCorrectMaybes1 (R1 x) = gCorrectMaybes1 x
+-- }
+class CorrectMaybes2 f where
+    gCorrectMaybes2 :: f a -> Bool
+
+instance (CorrectMaybes2 f, Datatype d) => CorrectMaybes2 (D1 d f) where
+    gCorrectMaybes2 z@(M1 x) =
+        if datatypeName z == "Maybe"
+        then gCorrectMaybes2 x
+        else True
+
+instance (Constructor d) => CorrectMaybes2 (C1 d f) where
+    gCorrectMaybes2 z@(M1 x) = conName z /= "Nothing"
+
+instance (CorrectMaybes2 f, CorrectMaybes2 g) => CorrectMaybes2 ((:+:) f g) where
+    gCorrectMaybes2 (L1 x) = gCorrectMaybes2 x
+    gCorrectMaybes2 (R1 x) = gCorrectMaybes2 x
+
